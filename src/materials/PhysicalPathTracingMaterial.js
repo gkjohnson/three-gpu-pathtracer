9147c09b0594e792970e188b79847774aafa75e3
Garrett Johnson <garrett.kjohnson@gmail.com>
2022-04-10 23:16:10 -0700

    comment update

--- src/materials/PhysicalPathTracingMaterial.js
+++ src/materials/PhysicalPathTracingMaterial.js
@@ -199,21 +199,24 @@ export class PhysicalPathTracingMaterial extends MaterialBase {
 
 						}
 
-						// possibly skip this sample if it's transparent or alpha test is enabled
-						// alpha test is disabled when it === 0
+						// possibly skip this sample if it's transparent, alpha test is enabled, or we hit the wrong material side
+						// and it's single sided.
+						// - alpha test is disabled when it === 0
+						// - the material sidedness test is complicated because we want light to pass through the back side but still
+						// be able to see the front side. This boolean checks if the side we hit is the front side on the first ray
+						// and we're rendering the other then we skip it. Do the opposite on subsequent bounces to get incoming light.
 						float alphaTest = material.alphaTest;
 						bool useAlphaTest = alphaTest != 0.0;
 						bool isFirstHit = i == 0;
 						if (
-							useAlphaTest && albedo.a < alphaTest
+							// material sidedness
+							material.side != 0.0 && ( side == material.side ) == isFirstHit
+
+							// alpha test
+							|| useAlphaTest && albedo.a < alphaTest
+
+							// opacity
 							|| ! useAlphaTest && albedo.a < rand()
-							|| (
-								material.side != 0.0 &&
-								(
-									( side == material.side && isFirstHit ) ||
-									( side != material.side && ! isFirstHit )
-								)
-							)
 						) {
 
 							vec3 point = rayOrigin + rayDirection * dist;
