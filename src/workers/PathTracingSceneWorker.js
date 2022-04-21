7fbf8cfb0e53f165f1c51353d0e7cf7f588ff89d
Garrett Johnson <garrett.kjohnson@gmail.com>
2022-04-21 10:34:57 -0700

    Update PathTracingSceneWorker.js

--- src/workers/PathTracingSceneWorker.js
+++ src/workers/PathTracingSceneWorker.js
@@ -1,3 +1,31 @@
-export class PathTracingSceneWorker {
-  
+import { PathTracingSceneGenerator } from '../core/PathTracingSceneGenerator.js';
+
+export class PathTracingSceneWorker extends PathTracingSceneGenerator {
+
+	constructor() {
+
+		this.bvhGenerator = new GenerateMeshBVHWorker();
+
+	}
+	
+	generate( scene, options = {} ) {
+
+		const { bvhGenerator } = this;
+		const { geometry, materials, textures } = this._prepScene( scene );
+
+		const bvhOptions = { strategy: SAH, ...options, maxLeafTris: 1 };
+		const bvhPromise = bvhGenerator.generate( geometry, bvhOptions );
+		return bvhPromise.then( bvh => {
+
+			return {
+				scene,
+				materials,
+				textures,
+				bvh,
+			};
+
+		} );
+
+	}
+
 }
