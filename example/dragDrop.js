2b66ac1288d69513932317ee30b9f38c2d77c6e5
Don McCurdy <dm@donmccurdy.com>
2022-04-29 10:58:21 -0400

    Update dragDrop demo to latest API.

--- example/dragDrop.js
+++ example/dragDrop.js
@@ -1,232 +1,138 @@
-import { ACESFilmicToneMapping, NoToneMapping, Box3, LoadingManager, EquirectangularReflectionMapping, PMREMGenerator, Sphere, Color, DoubleSide } from 'three';
-import { MeshoptDecoder } from 'three/examples/jsm/libs/meshopt_decoder.module.js';
+import * as THREE from 'three';
+import { FullScreenQuad } from 'three/examples/jsm/postprocessing/Pass.js';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+import { PathTracingRenderer, PhysicalPathTracingMaterial } from '../src/index.js';
+import { PathTracingSceneWorker } from '../src/workers/PathTracingSceneWorker.js';
 import { RGBELoader } from 'three/examples/jsm/loaders/RGBELoader.js';
-import { LDrawLoader } from 'three/examples/jsm/loaders/LDrawLoader.js';
-import { LDrawUtils } from 'three/examples/jsm/utils/LDrawUtils.js';
+import { MeshoptDecoder } from 'three/examples/jsm/libs/meshopt_decoder.module.js';
 import { GUI } from 'three/examples/jsm/libs/lil-gui.module.min.js';
-import { PathTracingViewer } from '../src/viewers/PathTracingViewer.js';
-import Stats from 'three/examples/jsm/libs/stats.module.js';
 
-import * as MikkTSpace from './lib/mikktspace.module.js';
 import { WebIO } from '@gltf-transform/core';
 import { ALL_EXTENSIONS } from '@gltf-transform/extensions';
 import { metalRough } from '@gltf-transform/functions';
 import { DocumentView, ImageProvider } from '@gltf-transform/view';
 import { getFilesFromDataTransferItems } from "@placemarkio/flat-drop-files";
 
-const creditEl = document.getElementById( 'credits' );
+let renderer, controls, scene, sceneInfo, ptRenderer, camera, fsQuad;
+
+let frameRequestID;
+
 const loadingEl = document.getElementById( 'loading' );
+const creditEl = document.getElementById( 'credits' );
 const samplesEl = document.getElementById( 'samples' );
 
-const viewer = new PathTracingViewer();
-viewer.init();
-viewer.domElement.style.width = '100%';
-viewer.domElement.style.height = '100%';
-document.body.appendChild( viewer.domElement );
+const params = {
+
+	environmentIntensity: 1,
+	environmentRotation: 0,
+	emissiveIntensity: 1,
+	bounces: 20,
+	samplesPerFrame: 1,
+	resolutionScale: 1 / window.devicePixelRatio,
+	filterGlossyFactor: 0.25,
+	tiles: 2,
+
+};
+
+// clamp value for mobile
+const aspectRatio = window.innerWidth / window.innerHeight;
+if ( aspectRatio < 0.65 ) {
+
+	params.bounces = Math.min( params.bounces, 10 );
+	params.resolutionScale *= 0.5;
+	params.tiles = 3;
+
+}
 
 const io = new WebIO()
 	.registerExtensions(ALL_EXTENSIONS)
 	.registerDependencies({'meshopt.decoder': MeshoptDecoder});
+
 const imageProvider = new ImageProvider();
 
-const envMaps = {
-	'Royal Esplanade': 'https://raw.githubusercontent.com/mrdoob/three.js/dev/examples/textures/equirectangular/royal_esplanade_1k.hdr',
-	'Moonless Golf': 'https://raw.githubusercontent.com/mrdoob/three.js/dev/examples/textures/equirectangular/moonless_golf_1k.hdr',
-	'Overpass': 'https://raw.githubusercontent.com/mrdoob/three.js/dev/examples/textures/equirectangular/pedestrian_overpass_1k.hdr',
-	'Venice Sunset': 'https://raw.githubusercontent.com/mrdoob/three.js/dev/examples/textures/equirectangular/venice_sunset_1k.hdr',
-};
+const generator = new PathTracingSceneWorker();
 
-const params = {
+//
 
-	acesToneMapping: true,
-	resolutionScale: 0.75 / window.devicePixelRatio,
-	tilesX: 2,
-	tilesY: 2,
-	samplesPerFrame: 1,
+init();
 
-	environment: 'ENVMAP',
-	envMap: envMaps[ 'Royal Esplanade' ],
+//
 
-	gradientTop: '#bfd8ff',
-	gradientBottom: '#ffffff',
+async function init() {
 
-	environmentIntensity: 3.0,
-	environmentBlur: 0.35,
+	renderer = new THREE.WebGLRenderer( { antialias: true } );
+	renderer.toneMapping = THREE.ACESFilmicToneMapping;
+	document.body.appendChild( renderer.domElement );
 
-	backgroundType: 'Gradient',
-	bgGradientTop: '#111111',
-	bgGradientBottom: '#000000',
+	scene = new THREE.Scene();
 
-	enable: true,
-	bounces: 3,
+	camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.025, 500 );
+	camera.position.set( 0.4, 0.6, 2.65 );
 
-	floorColor: '#080808',
-	floorEnabled: true,
+	ptRenderer = new PathTracingRenderer( renderer );
+	ptRenderer.camera = camera;
+	ptRenderer.material = new PhysicalPathTracingMaterial();
+	ptRenderer.tiles.set( params.tiles, params.tiles );
 
-};
+	fsQuad = new FullScreenQuad( new THREE.MeshBasicMaterial( {
+		map: ptRenderer.target.texture,
+	} ) );
 
-let gui = null;
-function buildGui() {
+	controls = new OrbitControls( camera, renderer.domElement );
+	controls.target.set( - 0.15, 0.33, - 0.08 );
+	camera.lookAt( controls.target );
+	controls.addEventListener( 'change', () => ptRenderer.reset() );
+	controls.update();
 
-	if ( gui ) {
+	//
 
-		gui.destroy();
+	const environmentTexture = await new RGBELoader()
+		.loadAsync( 'https://raw.githubusercontent.com/mrdoob/three.js/dev/examples/textures/equirectangular/royal_esplanade_1k.hdr');
 
-	}
+	const pmremGenerator = new THREE.PMREMGenerator( renderer );
+	pmremGenerator.compileCubemapShader();
 
-	gui = new GUI();
+	const envMap = pmremGenerator.fromEquirectangular( environmentTexture );
 
-	const resolutionFolder = gui.addFolder( 'resolution' );
-	resolutionFolder.add( params, 'resolutionScale', 0.1, 1.0, 0.01 ).onChange( v => {
+	ptRenderer.material.environmentMap = envMap.texture;
+	scene.environment = envMap.texture;
 
-		viewer.setScale( parseFloat( v ) );
+	//
 
-	} );
-	resolutionFolder.add( params, 'samplesPerFrame', 1, 10, 1 ).onChange( v => {
+	window.CAMERA = camera;
+	window.CONTROLS = controls;
 
-		viewer.samplesPerFrame = parseInt( v );
+	onResize();
+	window.addEventListener( 'resize', onResize );
 
-	} );
-	resolutionFolder.add( params, 'tilesX', 1, 10, 1 ).onChange( v => {
-
-		viewer.ptRenderer.tiles.x = parseInt( v );
-
-	} );
-	resolutionFolder.add( params, 'tilesY', 1, 10, 1 ).onChange( v => {
-
-		viewer.ptRenderer.tiles.y = parseInt( v );
-
-	} );
-	resolutionFolder.open();
-
-	const environmentFolder = gui.addFolder( 'environment' );
-	environmentFolder.add( params, 'envMap', envMaps ).name( 'map' ).onChange( updateEnvMap );
-	environmentFolder.add( params, 'environmentBlur', 0.0, 1.0, 0.01 ).onChange( v => {
-
-		viewer.ptRenderer.material.environmentBlur = parseFloat( v );
-		viewer.ptRenderer.reset();
-
-	} ).name( 'env map blur' );
-	environmentFolder.add( params, 'environmentIntensity', 0.0, 10.0, 0.01 ).onChange( v => {
-
-		viewer.ptRenderer.material.environmentIntensity = parseFloat( v );
-		viewer.ptRenderer.reset();
-
-	} ).name( 'intensity' );
-	environmentFolder.open();
-
-	const backgroundFolder = gui.addFolder( 'background' );
-	backgroundFolder.add( params, 'backgroundType', [ 'Environment', 'Gradient' ] ).onChange( v => {
-
-		viewer.ptRenderer.material.setDefine( 'GRADIENT_BG', Number( v === 'Gradient' ) );
-		if ( v === 'Gradient' ) {
-
-			viewer.scene.background = new Color( 0x060606 );
-
-		} else {
-
-			viewer.scene.background = viewer.scene.environment;
-
-		}
-
-		viewer.ptRenderer.reset();
-
-	} );
-	backgroundFolder.addColor( params, 'bgGradientTop' ).onChange( v => {
-
-		viewer.ptRenderer.material.uniforms.bgGradientTop.value.set( v );
-		viewer.ptRenderer.reset();
-
-	} );
-	backgroundFolder.addColor( params, 'bgGradientBottom' ).onChange( v => {
-
-		viewer.ptRenderer.material.uniforms.bgGradientBottom.value.set( v );
-		viewer.ptRenderer.reset();
-
-	} );
-	backgroundFolder.open();
-
-	const floorFolder = gui.addFolder( 'floor' );
-	floorFolder.add( params, 'floorEnabled' ).onChange( v => {
-
-		viewer.ptRenderer.material.setDefine( 'DISPLAY_FLOOR', Number( v ) );
-		viewer.ptRenderer.reset();
-
-	} );
-	floorFolder.addColor( params, 'floorColor' ).onChange( v => {
-
-		viewer.ptRenderer.material.uniforms.floorColor.value.set( v );
-		viewer.ptRenderer.reset();
-
-	} );
-
-	const pathTracingFolder = gui.addFolder( 'path tracing' );
-	pathTracingFolder.add( params, 'enable' ).onChange( v => {
-
-		viewer.enablePathTracing = v;
-
-	} );
-	pathTracingFolder.add( params, 'acesToneMapping' ).onChange( v => {
-
-		viewer.renderer.toneMapping = v ? ACESFilmicToneMapping : NoToneMapping;
-		viewer.fsQuad.material.needsUpdate = true;
-
-	} );
-	pathTracingFolder.add( params, 'bounces', 1, 20, 1 ).onChange( v => {
-
-		viewer.ptRenderer.material.setDefine( 'BOUNCES', parseInt( v ) );
-		viewer.ptRenderer.reset();
-
-	} );
-	pathTracingFolder.open();
+	initGUI();
 
 }
 
-function updateEnvMap() {
+function initGUI() {
 
-	new RGBELoader()
-		.load( params.envMap, texture => {
+	const gui = new GUI();
+	gui.add( params, 'tiles', 1, 4, 1 ).onChange( value => ptRenderer.tiles.set( value, value ) );
+	gui.add( params, 'samplesPerFrame', 1, 10, 1 );
+	gui.add( params, 'filterGlossyFactor', 0, 1 ).onChange( () => ptRenderer.reset() );
+	gui.add( params, 'environmentIntensity', 0, 25 ).onChange( () => ptRenderer.reset() );
+	gui.add( params, 'environmentRotation', 0, 40 ).onChange( v => {
 
-			if ( viewer.ptRenderer.material.environmentMap ) {
+		ptRenderer.material.environmentRotation.setFromMatrix4( new THREE.Matrix4().makeRotationY( v ) );
+		ptRenderer.reset();
 
-				viewer.ptRenderer.material.environmentMap.dispose();
-				viewer.scene.environment.dispose();
-
-			}
-
-			const pmremGenerator = new PMREMGenerator( viewer.renderer );
-			pmremGenerator.compileCubemapShader();
-
-			const envMap = pmremGenerator.fromEquirectangular( texture );
-
-			texture.mapping = EquirectangularReflectionMapping;
-			viewer.ptRenderer.material.environmentIntensity = parseFloat( params.environmentIntensity );
-			viewer.ptRenderer.material.environmentMap = envMap.texture;
-			viewer.scene.environment = texture;
-			if ( params.backgroundType !== 'Gradient' ) {
-
-				viewer.scene.background = texture;
-
-			}
-
-			viewer.ptRenderer.reset();
-
-		} );
+	} );
+	gui.add( params, 'bounces', 1, 30, 1 ).onChange( () => ptRenderer.reset() );
+	gui.add( params, 'resolutionScale', 0.1, 1 ).onChange( () => onResize() );
 
 }
 
 async function updateModel( modelDocument ) {
 
-	if ( gui ) {
-
-		gui.destroy();
-		gui = null;
-
-	}
-
-	await MikkTSpace.ready;
-
+	scene.clear();
 	imageProvider.clear();
+	ptRenderer.reset();
 
 	const modelRootDef = modelDocument.getRoot();
 	const modelSceneDef = modelRootDef.getDefaultScene() || modelRootDef.listScenes()[0];
@@ -249,54 +155,92 @@ async function updateModel( modelDocument ) {
 
 	// center the model
 
-	const box = new Box3();
+	model.updateMatrixWorld();
+
+	const box = new THREE.Box3();
 	box.setFromObject( model );
 	model.position
 		.addScaledVector( box.min, - 0.5 )
 		.addScaledVector( box.max, - 0.5 );
 
-	const sphere = new Sphere();
+	const sphere = new THREE.Sphere();
 	box.getBoundingSphere( sphere );
 
 	model.scale.setScalar( 1 / sphere.radius );
 	model.position.multiplyScalar( 1 / sphere.radius );
 
+	scene.add( model );
+
 	// load the model
 
-	await viewer.setModel( model, { onProgress: v => {
+	const result = await generator.generate( scene );
 
-		const percent = Math.floor( 100 * v );
-		loadingEl.innerText = `Building BVH : ${ percent }%`;
+	sceneInfo = result;
 
-	} } );
+	const { bvh, textures, materials } = result;
+	const geometry = bvh.geometry;
+	const material = ptRenderer.material;
+
+	material.bvh.updateFrom( bvh );
+	material.normalAttribute.updateFrom( geometry.attributes.normal );
+	material.tangentAttribute.updateFrom( geometry.attributes.tangent );
+	material.uvAttribute.updateFrom( geometry.attributes.uv );
+	material.materialIndexAttribute.updateFrom( geometry.attributes.materialIndex );
+	material.textures.setTextures( renderer, 2048, 2048, textures );
+	material.materials.updateFrom( materials, textures );
+	material.setDefine( 'MATERIAL_LENGTH', materials.length );
+
+	generator.dispose();
 
 	loadingEl.style.visibility = 'hidden';
 
-	buildGui();
-
-	viewer.pausePathTracing = false;
-	viewer.renderer.domElement.style.visibility = 'visible';
-	viewer.ptRenderer.material.uniforms.floorHeight.value = - ( box.max.y - box.min.y ) / ( 2 * sphere.radius );
+	animate();
 
 }
 
-const stats = new Stats();
-document.body.appendChild( stats.dom );
-viewer.renderer.physicallyCorrectLights = true;
-viewer.renderer.toneMapping = ACESFilmicToneMapping;
-viewer.ptRenderer.material.setDefine( 'GRADIENT_BG', 1 );
-viewer.scene.background = new Color( 0x060606 );
-viewer.ptRenderer.tiles.set( params.tilesX, params.tilesY );
-viewer.setScale( params.resolutionScale );
-viewer.onRender = () => {
+function onResize() {
 
-	stats.update();
-	const samples = Math.floor( viewer.ptRenderer.samples );
-	samplesEl.innerText = `samples: ${ samples }`;
+	const w = window.innerWidth;
+	const h = window.innerHeight;
+	const scale = params.resolutionScale;
+	const dpr = window.devicePixelRatio;
 
-};
+	ptRenderer.target.setSize( w * scale * dpr, h * scale * dpr );
+	ptRenderer.reset();
 
-updateEnvMap();
+	renderer.setSize( w, h );
+	renderer.setPixelRatio( window.devicePixelRatio * scale );
+	camera.aspect = w / h;
+	camera.updateProjectionMatrix();
+
+}
+
+function animate() {
+
+	frameRequestID = requestAnimationFrame( animate );
+
+	ptRenderer.material.materials.updateFrom( sceneInfo.materials, sceneInfo.textures );
+
+	ptRenderer.material.filterGlossyFactor = params.filterGlossyFactor;
+	ptRenderer.material.environmentIntensity = params.environmentIntensity;
+	ptRenderer.material.environmentBlur = 0.35;
+	ptRenderer.material.bounces = params.bounces;
+
+	camera.updateMatrixWorld();
+
+	for ( let i = 0, l = params.samplesPerFrame; i < l; i ++ ) {
+
+		ptRenderer.update();
+
+	}
+
+	renderer.autoClear = false;
+	fsQuad.render( renderer );
+	renderer.autoClear = true;
+
+	samplesEl.innerText = `Samples: ${ Math.floor( ptRenderer.samples ) }`;
+
+}
 
 // drag-and-drop implementation
 
@@ -316,8 +260,8 @@ document.body.addEventListener('drop', ( e ) => {
 
 	e.preventDefault();
 
-	viewer.pausePathTracing = true;
-	viewer.renderer.domElement.style.visibility = 'hidden';
+	cancelAnimationFrame( frameRequestID );
+
 	samplesEl.innerText = '--';
 	creditEl.innerText = '--';
 	loadingEl.innerText = 'Parsing';
@@ -345,4 +289,3 @@ document.body.addEventListener('drop', ( e ) => {
 	});
 
 });
-
