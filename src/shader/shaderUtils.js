f63289187ba871959e1ce4170cee2c96e0f455d2
Noeri Huisman <mrxz@users.noreply.github.com>
2022-06-15 10:22:56 +0200

    Added comment describing how intersectsRectangle works with reference

--- src/shader/shaderUtils.js
+++ src/shader/shaderUtils.js
@@ -320,20 +320,24 @@ export const shaderUtils = /* glsl */`
 
 	}
 
-	bool intersectsRectangle( vec3 position, vec3 normal, vec3 u, vec3 v, vec3 rayOrigin, vec3 rayDirection, out float dist ) {
+	// Finds the point where the ray intersects the plane defined by u and v and checks if this point
+	// falls in the bounds of the rectangle on that same plane.
+	// Plane intersection: https://lousodrome.net/blog/light/2020/07/03/intersection-of-a-ray-and-a-plane/
+	bool intersectsRectangle( vec3 center, vec3 normal, vec3 u, vec3 v, vec3 rayOrigin, vec3 rayDirection, out float dist ) {
 
-		float dt = dot( rayDirection, normal );
-		float t = ( dot( normal, position ) - dot( normal, rayOrigin ) ) / dt;
+		float t = dot( center - rayOrigin, normal ) / dot( rayDirection, normal );
 
 		if ( t > EPSILON ) {
 
-			vec3 hit = rayOrigin + rayDirection * t;
-			vec3 vi = hit - position;
+			vec3 p = rayOrigin + rayDirection * t;
+			vec3 vi = p - center;
+
+			// check if p falls inside the rectangle
 			float a1 = dot( u, vi );
-			if ( a1 >= - 0.5 && a1 <= 0.5 ) {
+			if ( abs( a1 ) <= 0.5 ) {
 
 				float a2 = dot( v, vi );
-				if ( a2 >= - 0.5 && a2 <= 0.5 ) {
+				if ( abs( a2 ) <= 0.5 ) {
 
 					dist = t;
 					return true;
