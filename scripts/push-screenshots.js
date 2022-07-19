7c1722bfa09f0a3b06f31965784f1547fbd17247
Garrett Johnson <garrett.kjohnson@gmail.com>
2022-07-18 23:17:14 -0700

    update screenshot maker

--- scripts/push-screenshots.js
+++ scripts/push-screenshots.js
@@ -7,29 +7,32 @@ import { execSync } from 'child_process';
 	const status = await git.status();
 	const currentBranch = status.current;
 
-	// if ( currentBranch !== 'main' ) {
+	if ( currentBranch !== 'main' ) {
 
-	// 	console.error( 'Current branch is not set to main.' );
-	// 	process.exit( 1 );
-
-	// }
-
-	const modified = status.modified.length + status.created.length + status.renamed.length + status.deleted.length;
-	if ( modified !== 0 ) {
-
-		console.error( 'Current branch is not clean.' );
+		console.error( 'Current branch is not set to main' );
 		process.exit( 1 );
 
 	}
 
+	const modified = status.modified.length + status.created.length + status.renamed.length + status.deleted.length;
+	if ( modified !== 0 ) {
+
+		console.error( 'Current branch is not clean' );
+		process.exit( 1 );
+
+	}
+
+	// switch and rebase branches
 	console.log( 'Switching to "screenshots" branch' );
 	await git.checkout( 'screenshots' );
 
-	console.log( 'Rebasing onto "main".' );
+	console.log( 'Rebasing onto "main"' );
 	await git.rebase( [ 'main', 'screenshots' ] );
 
-	// execSync( 'node ./scripts/update-screenshots.js' );
+	// rebuild the screenshots
+	execSync( 'node ./scripts/update-screenshots.js' );
 
+	// commit and push the screenshots
 	console.log( 'Committing all screenshots.' );
 	try {
 
@@ -42,12 +45,13 @@ import { execSync } from 'child_process';
 
 	} catch ( e ) {
 
-		console.error( 'Could not find any new files to commit.' );
+		console.error( 'Could not find any new files to commit' );
 		console.error( e.message );
 
 	}
 
-	console.log( `Switching back to "${ currentBranch }" branch.` );
+	// reset git
+	console.log( `Switching back to "${ currentBranch }" branch` );
 	await git.checkout( currentBranch );
 
 	process.exit();
