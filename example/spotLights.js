89b8f5ce79e756d57fb0b84ee1a8152e2c1f6c16
Garrett Johnson <garrett.kjohnson@gmail.com>
2022-07-27 22:20:23 -0700

    update spotlights example

--- example/spotLights.js
+++ example/spotLights.js
@@ -9,13 +9,11 @@ import { MeshoptDecoder } from 'three/examples/jsm/libs/meshopt_decoder.module.j
 import { GUI } from 'three/examples/jsm/libs/lil-gui.module.min.js';
 import { TransformControls } from 'three/examples/jsm/controls/TransformControls.js';
 
-let renderer, controls, transformControlsScene, spotLight1, lights, spotLights, spotLightHelpers, sceneInfo, ptRenderer, activeCamera, fsQuad, materials;
-let perspectiveCamera, orthoCamera;
+let renderer, controls, transformControlsScene, spotLight1, lights, spotLights, spotLightHelpers, sceneInfo, ptRenderer, fsQuad, materials;
+let perspectiveCamera;
 let envMap, envMapGenerator, scene;
 let iesTextures;
 let samplesEl;
-
-const orthoWidth = 5;
 
 const iesProfileURLs = [
 	'https://raw.githubusercontent.com/gkjohnson/3d-demo-data/main/ies/108b32f07d6d38a7a6528a6d307440df.ies',
@@ -27,30 +25,6 @@ const iesProfileURLs = [
 
 const params = {
 
-	material1: {
-		color: '#ffc766',
-		emissive: '#FFFFFF',
-		emissiveIntensity: 0.0,
-		roughness: 0.2,
-		metalness: 1.0,
-		ior: 1.495,
-		transmission: 0.0,
-		opacity: 1.0,
-		matte: false,
-		castShadow: true,
-	},
-	material2: {
-		color: '#db7157',
-		emissive: '#FFFFFF',
-		emissiveIntensity: 0,
-		roughness: 0.0,
-		metalness: 0.8,
-		transmission: 0.0,
-		ior: 1.495,
-		opacity: 1.0,
-		matte: false,
-		castShadow: true,
-	},
 	material3: {
 		color: '#3465a4',
 		roughness: 0.4,
@@ -91,25 +65,12 @@ const params = {
 	iesProfile: - 1,
 };
 
-if ( window.location.hash.includes( 'transmission' ) ) {
-
-	params.material1.metalness = 0.0;
-	params.material1.roughness = 0.05;
-	params.material1.transmission = 1.0;
-	params.material1.color = '#ffffff';
-	params.bounces = 10;
-
-}
-
 // adjust performance parameters for mobile
 const aspectRatio = window.innerWidth / window.innerHeight;
 if ( aspectRatio < 0.65 ) {
 
-	params.bounces = Math.max( params.bounces, 6 );
 	params.resolutionScale *= 0.5;
 	params.tiles = 2;
-	params.multipleImportanceSampling = false;
-	params.environmentBlur = 0.35;
 
 }
 
@@ -121,23 +82,17 @@ async function init() {
 	renderer.toneMapping = THREE.ACESFilmicToneMapping;
 	renderer.setClearColor( 0, 0 );
 	renderer.shadowMap.enabled = true;
-	//renderer.shadowMap.type = THREE.PCFSoftShadowMap;
 	document.body.appendChild( renderer.domElement );
 
 	const aspect = window.innerWidth / window.innerHeight;
 	perspectiveCamera = new PhysicalCamera( 75, aspect, 0.025, 500 );
-	perspectiveCamera.position.set( 16, 2, 15 );
-
-	const orthoHeight = orthoWidth / aspect;
-	orthoCamera = new THREE.OrthographicCamera( orthoWidth / - 2, orthoWidth / 2, orthoHeight / 2, orthoHeight / - 2, 0, 100 );
-	orthoCamera.position.set( - 4, 2, 3 );
+	perspectiveCamera.position.set( 0, 2, 15 );
 
 	ptRenderer = new PathTracingRenderer( renderer );
-	ptRenderer.alpha = true;
 	ptRenderer.material = new PhysicalPathTracingMaterial();
-	ptRenderer.material.setDefine( 'TRANSPARENT_TRAVERSALS', params.transparentTraversals );
 	ptRenderer.material.setDefine( 'FEATURE_MIS', Number( params.multipleImportanceSampling ) );
 	ptRenderer.tiles.set( params.tiles, params.tiles );
+	ptRenderer.camera = perspectiveCamera;
 
 	fsQuad = new FullScreenQuad( new THREE.MeshBasicMaterial( {
 		map: ptRenderer.target.texture,
@@ -145,7 +100,6 @@ async function init() {
 	} ) );
 
 	controls = new OrbitControls( perspectiveCamera, renderer.domElement );
-	controls.target.set( 16, 1, 1 );
 	controls.addEventListener( 'change', () => {
 
 		ptRenderer.reset();
@@ -177,7 +131,7 @@ async function init() {
 	const generator = new PathTracingSceneWorker();
 	const gltfPromise = new GLTFLoader()
 		.setMeshoptDecoder( MeshoptDecoder )
-		.loadAsync( 'https://raw.githubusercontent.com/gkjohnson/3d-demo-data/main/models/material-balls/material_ball_v2.glb' )
+		.loadAsync( 'https://raw.githubusercontent.com/gkjohnson/3d-demo-data/main/models/steampunk-robot/scene.gltf' )
 		.then( gltf => {
 
 			const group = new THREE.Group();
@@ -185,7 +139,7 @@ async function init() {
 			const gltfScenes = [ ];
 
 			// objects
-			gltf.scene.scale.setScalar( 0.01 );
+			gltf.scene.scale.setScalar( 1 );
 			gltf.scene.position.x = 0;
 			gltf.scene.updateMatrixWorld();
 			gltf.castShadow = true;
@@ -196,32 +150,8 @@ async function init() {
 			const box = new THREE.Box3();
 			box.setFromObject( gltf.scene );
 
-			gltf.scene = gltf.scene.clone();
-			gltf.scene.position.x += 8;
-			gltf.scene.updateMatrixWorld();
-			group.add( gltf.scene );
-			gltfScenes.push( gltf.scene );
-
-			gltf.scene = gltf.scene.clone();
-			gltf.scene.position.x += 8;
-			gltf.scene.updateMatrixWorld();
-			group.add( gltf.scene );
-			gltfScenes.push( gltf.scene );
-
-			gltf.scene = gltf.scene.clone();
-			gltf.scene.position.x += 8;
-			gltf.scene.updateMatrixWorld();
-			group.add( gltf.scene );
-			gltfScenes.push( gltf.scene );
-
-			gltf.scene = gltf.scene.clone();
-			gltf.scene.position.x += 8;
-			gltf.scene.updateMatrixWorld();
-			group.add( gltf.scene );
-			gltfScenes.push( gltf.scene );
-
 			const floor = new THREE.Mesh(
-				new THREE.CylinderBufferGeometry( 100, 100, 0.05, 200 ),
+				new THREE.CylinderBufferGeometry( 15, 15, 0.05, 200 ),
 				new THREE.MeshStandardMaterial( { color: 0xffffff, roughness: 0.5, metalness: 0.2 } ),
 			);
 			floor.geometry = floor.geometry.toNonIndexed();
@@ -231,7 +161,7 @@ async function init() {
 			group.add( floor );
 
 			const wall = new THREE.Mesh(
-				new THREE.BoxGeometry( 100, 10, 1 ),
+				new THREE.BoxGeometry( 15, 10, 1 ),
 				new THREE.MeshStandardMaterial( { color: 0xffffff, roughness: 0, metalness: 1.0 } ),
 			);
 			wall.castShadow = true;
@@ -278,7 +208,7 @@ async function init() {
 			const iesPromises = [];
 
 			const decays = [ 0, 1.5, 0, 0.25, 0 ];
-			for ( let i = 0; i < 5; ++ i ) {
+			for ( let i = 0; i < 1; ++ i ) {
 
 				const spotLight = new PhysicalSpotLight( 0xffffff );
 
@@ -343,43 +273,7 @@ async function init() {
 
 			}
 
-			// materials
-			const material1 = new THREE.MeshStandardMaterial();
-			const material2 = new THREE.MeshStandardMaterial();
-
-			gltfScenes.forEach( gltfScene => {
-
-				gltfScene.traverse( c => {
-
-					// the vertex normals on the material ball are off...
-					// TODO: precompute the vertex normals so they are correct on load
-					if ( c.geometry ) {
-
-						c.geometry.computeVertexNormals();
-
-					}
-
-					if ( c.name === 'Sphere_1' ) {
-
-						c.material = material2;
-
-					} else {
-
-						c.material = material1;
-
-					}
-
-					if ( c.name === 'subsphere_1' ) {
-
-						c.material = material2;
-
-					}
-
-				} );
-
-			} );
-
-			materials = [ material1, material2, floor.material, wall.material ];
+			materials = [ floor.material, wall.material ];
 
 			return Promise.all( [ generator.generate( group ), Promise.all( iesPromises ) ] );
 
@@ -421,15 +315,7 @@ async function init() {
 	window.addEventListener( 'resize', onResize );
 	const gui = new GUI();
 
-	updateCamera( params.cameraProjection );
-
 	const ptFolder = gui.addFolder( 'Path Tracing' );
-	ptFolder.add( params, 'acesToneMapping' ).onChange( value => {
-
-		renderer.toneMapping = value ? THREE.ACESFilmicToneMapping : THREE.NoToneMapping;
-		fsQuad.material.needsUpdate = true;
-
-	} );
 	ptFolder.add( params, 'stableNoise' ).onChange( value => {
 
 		ptRenderer.stableNoise = value;
@@ -457,12 +343,6 @@ async function init() {
 		ptRenderer.reset();
 
 	} );
-	ptFolder.add( params, 'transparentTraversals', 0, 40, 1 ).onChange( value => {
-
-		ptRenderer.material.setDefine( 'TRANSPARENT_TRAVERSALS', value );
-		ptRenderer.reset();
-
-	} );
 	ptFolder.add( params, 'resolutionScale', 0.1, 1 ).onChange( () => {
 
 		onResize();
@@ -472,12 +352,6 @@ async function init() {
 	const envFolder = gui.addFolder( 'Environment' );
 	envFolder.add( params, 'environmentIntensity', 0, 10 ).onChange( () => {
 
-		ptRenderer.reset();
-
-	} );
-	envFolder.add( params, 'environmentRotation', 0, 2 * Math.PI ).onChange( v => {
-
-		ptRenderer.material.environmentRotation.setFromMatrix4( new THREE.Matrix4().makeRotationY( v ) );
 		ptRenderer.reset();
 
 	} );
@@ -496,27 +370,9 @@ async function init() {
 		ptRenderer.reset();
 
 	} );
-	envFolder.add( params, 'checkerboardTransparency' ).onChange( v => {
-
-		if ( v ) {
-
-			document.body.classList.add( 'checkerboard' );
-
-		} else {
-
-			document.body.classList.remove( 'checkerboard' );
-
-		}
-
-	} );
 	envFolder.add( params, 'showTransformControls' );
 
 	const cameraFolder = gui.addFolder( 'Camera' );
-	cameraFolder.add( params, 'cameraProjection', [ 'Perspective', 'Orthographic' ] ).onChange( v => {
-
-		updateCamera( v );
-
-	} );
 	cameraFolder.add( perspectiveCamera, 'focusDistance', 1, 100 ).onChange( reset );
 	cameraFolder.add( perspectiveCamera, 'apertureBlades', 0, 10, 1 ).onChange( function ( v ) {
 
@@ -535,32 +391,6 @@ async function init() {
 		reset();
 
 	} ).listen();
-
-	const matFolder1 = gui.addFolder( 'Shell Material' );
-	matFolder1.addColor( params.material1, 'color' ).onChange( reset );
-	matFolder1.addColor( params.material1, 'emissive' ).onChange( reset );
-	matFolder1.add( params.material1, 'emissiveIntensity', 0.0, 50.0, 0.01 ).onChange( reset );
-	matFolder1.add( params.material1, 'roughness', 0, 1 ).onChange( reset );
-	matFolder1.add( params.material1, 'metalness', 0, 1 ).onChange( reset );
-	matFolder1.add( params.material1, 'opacity', 0, 1 ).onChange( reset );
-	matFolder1.add( params.material1, 'transmission', 0, 1 ).onChange( reset );
-	matFolder1.add( params.material1, 'ior', 0.9, 3.0 ).onChange( reset );
-	matFolder1.add( params.material1, 'matte' ).onChange( reset );
-	matFolder1.add( params.material1, 'castShadow' ).onChange( reset );
-	matFolder1.close();
-
-	const matFolder2 = gui.addFolder( 'Ball Material' );
-	matFolder2.addColor( params.material2, 'color' ).onChange( reset );
-	matFolder2.addColor( params.material2, 'emissive' ).onChange( reset );
-	matFolder2.add( params.material2, 'emissiveIntensity', 0.0, 50.0, 0.01 ).onChange( reset );
-	matFolder2.add( params.material2, 'roughness', 0, 1 ).onChange( reset );
-	matFolder2.add( params.material2, 'metalness', 0, 1 ).onChange( reset );
-	matFolder2.add( params.material2, 'opacity', 0, 1 ).onChange( reset );
-	matFolder2.add( params.material2, 'transmission', 0, 1 ).onChange( reset );
-	matFolder2.add( params.material2, 'ior', 0.9, 3.0 ).onChange( reset );
-	matFolder2.add( params.material2, 'matte' ).onChange( reset );
-	matFolder2.add( params.material2, 'castShadow' ).onChange( reset );
-	matFolder2.close();
 
 	const matFolder3 = gui.addFolder( 'Floor Material' );
 	matFolder3.addColor( params.material3, 'color' ).onChange( reset );
@@ -619,11 +449,6 @@ function onResize() {
 	perspectiveCamera.aspect = aspect;
 	perspectiveCamera.updateProjectionMatrix();
 
-	const orthoHeight = orthoWidth / aspect;
-	orthoCamera.top = orthoHeight / 2;
-	orthoCamera.bottom = orthoHeight / - 2;
-	orthoCamera.updateProjectionMatrix();
-
 }
 
 function reset() {
@@ -641,69 +466,16 @@ function updateEnvBlur() {
 
 }
 
-function updateCamera( cameraProjection ) {
-
-	if ( cameraProjection === 'Perspective' ) {
-
-		if ( activeCamera ) {
-
-			perspectiveCamera.position.copy( activeCamera.position );
-
-		}
-
-		activeCamera = perspectiveCamera;
-
-	} else {
-
-		if ( activeCamera ) {
-
-			orthoCamera.position.copy( activeCamera.position );
-
-		}
-
-		activeCamera = orthoCamera;
-
-	}
-
-	controls.object = activeCamera;
-	ptRenderer.camera = activeCamera;
-
-	controls.update();
-
-	reset();
-
-}
-
 function animate() {
 
 	requestAnimationFrame( animate );
 
-	const m1 = materials[ 0 ];
-	m1.color.set( params.material1.color ).convertSRGBToLinear();
-	m1.emissive.set( params.material1.emissive ).convertSRGBToLinear();
-	m1.emissiveIntensity = params.material1.emissiveIntensity;
-	m1.metalness = params.material1.metalness;
-	m1.roughness = params.material1.roughness;
-	m1.transmission = params.material1.transmission;
-	m1.ior = params.material1.ior;
-	m1.opacity = params.material1.opacity;
-
-	const m2 = materials[ 1 ];
-	m2.color.set( params.material2.color ).convertSRGBToLinear();
-	m2.emissive.set( params.material2.emissive ).convertSRGBToLinear();
-	m2.emissiveIntensity = params.material2.emissiveIntensity;
-	m2.metalness = params.material2.metalness;
-	m2.roughness = params.material2.roughness;
-	m2.transmission = params.material2.transmission;
-	m2.ior = params.material2.ior;
-	m2.opacity = params.material2.opacity;
-
-	const m3 = materials[ 2 ];
+	const m3 = materials[ 0 ];
 	m3.color.set( params.material3.color ).convertSRGBToLinear();
 	m3.metalness = params.material3.metalness;
 	m3.roughness = params.material3.roughness;
 
-	const m4 = materials[ 3 ];
+	const m4 = materials[ 1 ];
 	m4.color.set( params.material4.color ).convertSRGBToLinear();
 	m4.metalness = params.material4.metalness;
 	m4.roughness = params.material4.roughness;
@@ -713,36 +485,17 @@ function animate() {
 	m4.opacity = params.material4.opacity;
 
 	ptRenderer.material.materials.updateFrom( sceneInfo.materials, sceneInfo.textures );
-	ptRenderer.material.materials.setMatte( 0, params.material1.matte );
-	ptRenderer.material.materials.setMatte( 1, params.material2.matte );
-	ptRenderer.material.materials.setMatte( 2, params.material3.matte );
-	ptRenderer.material.materials.setMatte( 3, params.material4.matte );
-	ptRenderer.material.materials.setCastShadow( 0, params.material1.castShadow );
-	ptRenderer.material.materials.setCastShadow( 1, params.material2.castShadow );
-	ptRenderer.material.materials.setCastShadow( 2, params.material3.castShadow );
-	ptRenderer.material.materials.setCastShadow( 3, params.material4.castShadow );
 
 	ptRenderer.material.filterGlossyFactor = params.filterGlossyFactor;
 	ptRenderer.material.environmentIntensity = params.environmentIntensity;
 	ptRenderer.material.backgroundBlur = params.backgroundBlur;
 	ptRenderer.material.bounces = params.bounces;
-	ptRenderer.material.backgroundAlpha = params.backgroundAlpha;
-	ptRenderer.material.physicalCamera.updateFrom( activeCamera );
+	ptRenderer.material.physicalCamera.updateFrom( perspectiveCamera );
 
 	ptRenderer.material.lights.updateFrom( lights );
 	ptRenderer.material.spotLights.updateFrom( spotLights, iesTextures );
 
-	activeCamera.updateMatrixWorld();
-
-	if ( params.backgroundAlpha < 1.0 ) {
-
-		scene.background = null;
-
-	} else {
-
-		scene.background = scene.environment;
-
-	}
+	perspectiveCamera.updateMatrixWorld();
 
 	spotLightHelpers.forEach( spotLightHelper => {
 
@@ -758,7 +511,7 @@ function animate() {
 
 	if ( ptRenderer.samples < 1 ) {
 
-		renderer.render( scene, activeCamera );
+		renderer.render( scene, perspectiveCamera );
 
 	}
 
@@ -769,7 +522,7 @@ function animate() {
 	fsQuad.render( renderer );
 
 	if ( params.showTransformControls )
-		renderer.render( transformControlsScene, activeCamera );
+		renderer.render( transformControlsScene, perspectiveCamera );
 
 	renderer.autoClear = true;
 
