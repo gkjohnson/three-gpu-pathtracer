ab5f5eff835fd4da7a12d8382fdc809a42e8aa9f
Garrett Johnson <garrett.kjohnson@gmail.com>
2023-02-17 21:40:03 +0900

    Separate attenuate hit

--- src/materials/pathtracing/PhysicalPathTracingMaterial.js
+++ src/materials/pathtracing/PhysicalPathTracingMaterial.js
@@ -40,6 +40,9 @@ import { sobolCommonGLSL, sobolSamplingGLSL } from '../../shader/rand/sobol.glsl
 
 // path tracer utils
 import { cameraUtilsGLSL } from './glsl/cameraUtils.glsl.js';
+import { materialUtilsGLSL } from './glsl/materialUtils.glsl.js';
+import { attenuateHitGLSL } from './glsl/attenuateHit.glsl.js';
+import { materialToSurfaceRecGLSL } from './glsl/materialToSurfaceRec.glsl.js';
 
 export class PhysicalPathTracingMaterial extends MaterialBase {
 
@@ -202,6 +205,7 @@ export class PhysicalPathTracingMaterial extends MaterialBase {
 				varying vec2 vUv;
 
 				${ cameraUtilsGLSL }
+				${ attenuateHitGLSL }
 
 				float applyFilteredGlossy( float roughness, float accumulatedRoughness ) {
 
@@ -231,157 +235,15 @@ export class PhysicalPathTracingMaterial extends MaterialBase {
 
 				}
 
-				// step through multiple surface hits and accumulate color attenuation based on transmissive surfaces
-				bool attenuateHit(
-					BVH bvh, vec3 rayOrigin, vec3 rayDirection, float rayDist,
-					int traversals, int transparentTraversals, bool isShadowRay, out vec3 color
-				) {
-
-					// hit results
-					uvec4 faceIndices = uvec4( 0u );
-					vec3 faceNormal = vec3( 0.0, 0.0, 1.0 );
-					vec3 barycoord = vec3( 0.0 );
-					float side = 1.0;
-					float dist = 0.0;
-
-					color = vec3( 1.0 );
-
-					// TODO: we should be using sobol sampling here instead of rand but the sobol bounce and path indices need to be incremented
-					// and then reset.
-					for ( int i = 0; i < traversals; i ++ ) {
-
-						if ( bvhIntersectFirstHit( bvh, rayOrigin, rayDirection, faceIndices, faceNormal, barycoord, side, dist ) ) {
-
-							if ( dist > rayDist ) {
-
-								return true;
-
-							}
-
-							// TODO: attenuate the contribution based on the PDF of the resulting ray including refraction values
-							// Should be able to work using the material BSDF functions which will take into account specularity, etc.
-							// TODO: should we account for emissive surfaces here?
-
-							vec2 uv = textureSampleBarycoord( attributesArray, ATTR_UV, barycoord, faceIndices.xyz ).xy;
-							vec4 vertexColor = textureSampleBarycoord( attributesArray, ATTR_COLOR, barycoord, faceIndices.xyz );
-
-							uint materialIndex = uTexelFetch1D( materialIndexAttribute, faceIndices.x ).r;
-							Material material = readMaterialInfo( materials, materialIndex );
-
-							// adjust the ray to the new surface
-							bool isEntering = side == 1.0;
-							rayOrigin = stepRayOrigin( rayOrigin, rayDirection, - faceNormal, dist );
-
-							if ( ! material.castShadow && isShadowRay ) {
-
-								continue;
-
-							}
-
-							// Opacity Test
-
-							// albedo
-							vec4 albedo = vec4( material.color, material.opacity );
-							if ( material.map != - 1 ) {
-
-								vec3 uvPrime = material.mapTransform * vec3( uv, 1 );
-								albedo *= texture2D( textures, vec3( uvPrime.xy, material.map ) );
-
-							}
-
-							if ( material.vertexColors ) {
-
-								albedo *= vertexColor;
-
-							}
-
-							// alphaMap
-							if ( material.alphaMap != - 1 ) {
-
-								albedo.a *= texture2D( textures, vec3( uv, material.alphaMap ) ).x;
-
-							}
-
-							// transmission
-							float transmission = material.transmission;
-							if ( material.transmissionMap != - 1 ) {
-
-								vec3 uvPrime = material.transmissionMapTransform * vec3( uv, 1 );
-								transmission *= texture2D( textures, vec3( uvPrime.xy, material.transmissionMap ) ).r;
-
-							}
-
-							// metalness
-							float metalness = material.metalness;
-							if ( material.metalnessMap != - 1 ) {
-
-								vec3 uvPrime = material.metalnessMapTransform * vec3( uv, 1 );
-								metalness *= texture2D( textures, vec3( uvPrime.xy, material.metalnessMap ) ).b;
-
-							}
-
-							float alphaTest = material.alphaTest;
-							bool useAlphaTest = alphaTest != 0.0;
-							float transmissionFactor = ( 1.0 - metalness ) * transmission;
-							if (
-								transmissionFactor < rand() && ! (
-									// material sidedness
-									material.side != 0.0 && side == material.side
-
-									// alpha test
-									|| useAlphaTest && albedo.a < alphaTest
-
-									// opacity
-									|| material.transparent && ! useAlphaTest && albedo.a < rand()
-								)
-							) {
-
-								return true;
-
-							}
-
-							if ( side == 1.0 && isEntering ) {
-
-								// only attenuate by surface color on the way in
-								color *= mix( vec3( 1.0 ), albedo.rgb, transmissionFactor );
-
-							} else if ( side == - 1.0 ) {
-
-								// attenuate by medium once we hit the opposite side of the model
-								color *= transmissionAttenuation( dist, material.attenuationColor, material.attenuationDistance );
-
-							}
-
-							bool isTransmissiveRay = dot( rayDirection, faceNormal * side ) < 0.0;
-							if ( ( isTransmissiveRay || isEntering ) && transparentTraversals > 0 ) {
-
-								transparentTraversals --;
-								i --;
-
-							}
-
-
-						} else {
-
-							return false;
-
-						}
-
-					}
-
-					return true;
-
-				}
-
 				void main() {
 
+					// init
 					rng_initialize( gl_FragCoord.xy, seed );
 					sobolPixelIndex = ( uint( gl_FragCoord.x ) << 16 ) | ( uint( gl_FragCoord.y ) );
 					sobolPathIndex = uint( seed );
 
-					vec3 rayDirection;
-					vec3 rayOrigin;
-
+					// get camera ray
+					vec3 rayDirection, rayOrigin;
 					getCameraRay( rayDirection, rayOrigin );
 
 					// inverse environment rotation
@@ -403,11 +265,11 @@ export class PhysicalPathTracingMaterial extends MaterialBase {
 					float accumulatedRoughness = 0.0;
 					float accumulatedClearcoatRoughness = 0.0;
 					bool transmissiveRay = true;
+					bool isShadowRay = false;
 					int transparentTraversals = transmissiveBounces;
 					vec3 throughputColor = vec3( 1.0 );
 					SampleRec sampleRec;
 					int i;
-					bool isShadowRay = false;
 
 					for ( i = 0; i < bounces; i ++ ) {
 
