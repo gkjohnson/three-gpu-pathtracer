fe65705603fb5de2349ccfcb259a805c3bb33fbd
Garrett Johnson <garrett.kjohnson@gmail.com>
2023-02-26 12:23:58 +0900

    Update the fog material

--- src/materials/surface/FogVolumeMaterial.js
+++ src/materials/surface/FogVolumeMaterial.js
@@ -1,24 +1,20 @@
-import { MeshStandardMaterial } from 'three';
+import { Color, MeshStandardMaterial } from 'three';
 
 export class FogVolumeMaterial extends MeshStandardMaterial {
-
-	get density() {
-
-		return this.opacity;
-
-	}
-
-	set density( v ) {
-
-		this.opacity = v;
-
-	}
 
 	constructor( params ) {
 
 		super( params );
 
 		this.isFogVolumeMaterial = true;
+
+		this.density = 0.015;
+		this.emissive = new Color();
+		this.emissiveIntensity = 0.0;
+		this.opacity = 0.15;
+		this.transparent = true;
+		this.roughness = 1.0;
+		this.metalness = 0.0;
 
 	}
 
