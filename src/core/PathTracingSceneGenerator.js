7b16b38b90a8072133eee3d7fe21555bf68922fc
Garrett Johnson <garrett.kjohnson@gmail.com>
2024-02-10 13:29:41 +0900

    Handle inverted scales

--- src/core/PathTracingSceneGenerator.js
+++ src/core/PathTracingSceneGenerator.js
@@ -1,10 +1,37 @@
+import { Mesh } from 'three';
 import { DynamicPathTracingSceneGenerator } from './DynamicPathTracingSceneGenerator.js';
 
 export class PathTracingSceneGenerator {
 
 	generate( scene, options = {} ) {
 
-		const generator = new DynamicPathTracingSceneGenerator( scene );
+		// handle negative scales
+		const objects = [];
+		const scenes = Array.isArray( scene ) ? scene : [ scene ];
+		scenes.forEach( s => {
+
+			s.traverseVisible( c => {
+
+				if ( c.isMesh ) {
+
+					const o = new Mesh( c.geometry, c.material );
+					o.matrixWorld.copy( c.matrixWorld );
+					objects.push( o );
+
+					if ( c.matrixWorld.determinant() < 0 ) {
+
+						o.geometry = o.geometry.clone();
+						o.geometry.index.array.reverse();
+
+					}
+
+				}
+
+			} );
+
+		} );
+
+		const generator = new DynamicPathTracingSceneGenerator( objects );
 		generator.bvhOptions = options;
 		return generator.generate();
 
