27e45da094ad38a9a6411c51d6663c89cb26a6ab
Garrett Johnson <garrett.kjohnson@gmail.com>
2024-02-10 19:18:53 +0900

    Complete the stratified texture

--- src/uniforms/StratifiedSamplesTexture.js
+++ src/uniforms/StratifiedSamplesTexture.js
@@ -3,27 +3,42 @@ import { makeStratifiedSamplerCombined } from './stratified/StratifiedSamplerCom
 
 export class StratifiedSamplesTexture extends DataTexture {
 
-	constructor() {
+	constructor( count = 1, depth = 1, strata = 1 ) {
 
 		super( new Float32Array( 1 ), 1, 1, RGBAFormat, FloatType );
 		this.minFilter = NearestFilter;
 		this.magFilter = NearestFilter;
 
+		this.strata = - 1;
 		this.sampler = null;
+
+		this.init( count, depth, strata );
 
 	}
 
-	init( count, strata ) {
+	init( count, depth, strata ) {
 
-		const dimensions = new Array( count ).fill( 4 );
-		this.sampler = new makeStratifiedSamplerCombined( strata, dimensions );
+		const { image } = this;
+		image.width = depth;
+		image.height = count;
+		image.data = new Float32Array( depth * count * 4 );
+
+		const dimensions = new Array( count * depth ).fill( 4 );
+		this.sampler = makeStratifiedSamplerCombined( strata, dimensions );
+		this.next();
 
 	}
 
 	next() {
 
-		const data = this.sampler.next();
-		// regenerate the new data, wrap around if needed
+		const data = this.image.data;
+		const samples = this.sampler.next();
+		const strata = this.sampler.strataCount;
+		for ( let i = 0; i < samples.length; i ++ ) {
+
+			data[ i ] = samples[ i ] / strata;
+
+		}
 
 		this.needsUpdate = true;
 
