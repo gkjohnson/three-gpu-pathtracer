daf7c150bb2fca3ed1267c45adc6abf206cb4f68
Garrett Johnson <garrett.kjohnson@gmail.com>
2024-04-04 22:50:04 +0900

    Remove unused variables

--- src/core/WebGLPathTracer.js
+++ src/core/WebGLPathTracer.js
@@ -1,4 +1,4 @@
-import { PerspectiveCamera, Scene, Vector2, Clock, NormalBlending, NoBlending, AdditiveBlending, FloatType, HalfFloatType } from 'three';
+import { PerspectiveCamera, Scene, Vector2, Clock, NormalBlending, NoBlending, AdditiveBlending } from 'three';
 import { PathTracingSceneGenerator } from './PathTracingSceneGenerator.js';
 import { PathTracingRenderer } from './PathTracingRenderer.js';
 import { FullScreenQuad } from 'three/examples/jsm/postprocessing/Pass.js';
