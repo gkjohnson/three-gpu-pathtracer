91757b0d243e1439b7f668a7261d19470e0796bd
Garrett Johnson <garrett.kjohnson@gmail.com>
2026-03-02 11:21:22 +0900

    Fix imports

--- src/webgpu/WebGPUPathTracer.js
+++ src/webgpu/WebGPUPathTracer.js
@@ -1,4 +1,4 @@
-import { DataTexture, LinearFilter, Vector2, Scene, PerspectiveCamera, Color, NoToneMapping, RenderTarget, FloatType, Timer, StorageTexture } from 'three/webgpu';
+import { DataTexture, LinearFilter, Vector2, Scene, PerspectiveCamera, Color, NoToneMapping, FloatType, Timer, StorageTexture } from 'three/webgpu';
 import { MeshBVH, SAH } from 'three-mesh-bvh';
 import { FullScreenQuad } from 'three/examples/jsm/postprocessing/Pass.js';
 import { RenderToScreenNodeMaterial } from './materials/RenderToScreenMaterial.js';
