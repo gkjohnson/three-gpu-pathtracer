cec151123f5320c55df2a5a847f24bc21c967e6a
Garrett Johnson <garrett.kjohnson@gmail.com>
2026-03-11 15:20:47 +0900

    Use custom filtering

--- src/webgpu/materials/RenderToScreenMaterial.js
+++ src/webgpu/materials/RenderToScreenMaterial.js
@@ -1,5 +1,34 @@
-import { MeshBasicNodeMaterial, NoToneMapping, StorageTexture } from 'three/webgpu';
+import { FloatType, MeshBasicNodeMaterial, NearestFilter, NoToneMapping, StorageTexture } from 'three/webgpu';
 import { uv, varying, texture, vec4, toneMapping, uniform, wgslFn } from 'three/tsl';
+
+// TODO: we could fall back to hardware-based filtering if available but it has to be specifically
+// requested and available on the renderer which we don't have access to immediately. It's possible this
+// can be detected during build time via a custom node to adjust the sample approach used?
+const sampleTexelFn = wgslFn( /* wgsl */`
+	fn sampleTexel( tex: texture_2d<f32>, coord: vec2f ) -> vec4f {
+
+		// Manual bilinear filtering using textureLoad to support filterable float32 textures
+		// on all devices
+		let size = vec2f( textureDimensions( tex, 0 ) );
+		let pxCoord = coord * size - 0.5;
+		let px = vec2i( floor( pxCoord ) );
+		let fr = fract( pxCoord );
+
+		// get the four sibling samples
+		let s00 = textureLoad( tex, clamp( px,                 vec2i( 0 ), vec2i( size ) - 1 ), 0 );
+		let s10 = textureLoad( tex, clamp( px + vec2i( 1, 0 ), vec2i( 0 ), vec2i( size ) - 1 ), 0 );
+		let s01 = textureLoad( tex, clamp( px + vec2i( 0, 1 ), vec2i( 0 ), vec2i( size ) - 1 ), 0 );
+		let s11 = textureLoad( tex, clamp( px + vec2i( 1, 1 ), vec2i( 0 ), vec2i( size ) - 1 ), 0 );
+
+		// interpolate on the x axis
+		let y0 = mix( s00, s10, fr.x );
+		let y1 = mix( s01, s11, fr.x );
+
+		// then y
+		return mix( y0, y1, fr.y );
+
+	}
+` );
 
 // Material to apply tone mapping _before_ applying alpha blending
 export class RenderToScreenNodeMaterial extends MeshBasicNodeMaterial {
@@ -74,6 +103,8 @@ export class RenderToScreenNodeMaterial extends MeshBasicNodeMaterial {
 		const fromTexNode = texture( new StorageTexture(), varying( uv() ) );
 		this._fromTexNode = fromTexNode;
 
+		const texUV = varying( uv() );
+
 		const transitionUniform = uniform( 1.0 );
 		this._transitionUniform = transitionUniform;
 
@@ -84,8 +115,8 @@ export class RenderToScreenNodeMaterial extends MeshBasicNodeMaterial {
 
 			}
 		` )( {
-			col0: fromTexNode,
-			col1: texNode,
+			col0: sampleTexelFn( { tex: fromTexNode, coord: texUV } ),
+			col1: sampleTexelFn( { tex: texNode, coord: texUV } ),
 			transition: transitionUniform,
 		} );
 
