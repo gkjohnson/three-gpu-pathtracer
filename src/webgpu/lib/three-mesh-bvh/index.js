0ad6e75cb41a8c4cbda04f95c582f72276e9dd74
Garrett Johnson <garrett.kjohnson@gmail.com>
2026-07-04 02:42:19 +0900

    Remove unnecessary exports

--- src/webgpu/lib/three-mesh-bvh/index.js
+++ src/webgpu/lib/three-mesh-bvh/index.js
@@ -6,14 +6,6 @@ export * from './tsl/structs.js';
 export * from './tsl/fns.js';
 export * from './tsl/constants.js';
 
-export * from './shapecastFns/getShapecastFn.js';
-export * from './shapecastFns/getRaycastFirstHitFn.js';
-export * from './shapecastFns/getSampleTrianglePointFn.js';
-export * from './shapecastFns/getClosestPointToPointFn.js';
-
-export * from './utils/packBVHBufferUtils.js';
-export * from './utils/toObjectBVH.js';
-
 // temporary exports
 export * from './nodes/NodeProxy.js';
 export * from './nodes/WGSLTagFnNode.js';
