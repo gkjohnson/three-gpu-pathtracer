8b15636d756dc126a00c0c1f460f63ce7f227e81
Garrett Johnson <garrett.kjohnson@gmail.com>
2026-07-05 11:15:18 +0900

    Add comment

--- src/webgpu/index.js
+++ src/webgpu/index.js
@@ -1,5 +1,6 @@
 export * from './WebGPUPathTracer.js';
 export * from './BlurredEnvMapGenerator.js';
 
+// extend the cameras to avoid adding WebGPU imports to the WebGLPathTracer
 import './shims/EquirectCameraShim.js';
 import './shims/PhysicalCameraShim.js';
