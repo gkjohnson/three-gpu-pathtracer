eaeac142b50674b6bbc57cf3adabf5ff4d1082a6
Garrett Johnson <garrett.kjohnson@gmail.com>
2026-07-11 08:47:50 +0900

    Update alpha test rng index

--- src/webgpu/nodes/random.wgsl.js
+++ src/webgpu/nodes/random.wgsl.js
@@ -7,7 +7,7 @@ export const RNG_INDEX_ENVIRONMENT_SAMPLE = 1;
 export const RNG_INDEX_SCATTER_TYPE = 2;
 export const RNG_INDEX_SCATTER_DIRECTION = 3;
 export const RNG_INDEX_APERTURE_SAMPLE = 4;
-export const RNG_INDEX_ALPHA_TEST = 5;
+export const RNG_INDEX_ALPHA_TEST = 1000;
 
 export const pcgStateStruct = wgsl( /* wgsl */`
 
