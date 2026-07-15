8b2c7afc2222df33b66d5888af0c50ba55a0e58a
Garrett Johnson <garrett.kjohnson@gmail.com>
2026-07-15 14:59:11 +0900

    Add golden ratio offset to sequence

--- src/webgpu/nodes/rand/bluedither.wgsl.js
+++ src/webgpu/nodes/rand/bluedither.wgsl.js
@@ -1,4 +1,4 @@
-import { float, texture, vec4 } from 'three/tsl';
+import { texture, uint, vec4 } from 'three/tsl';
 import { wgslTagFn } from 'three-mesh-bvh/webgpu';
 import { rand4, rngInit, rngNextBounce } from './sobol.wgsl.js';
 import { BlueNoiseTexture } from '../../../textures/BlueNoiseTexture.js';
@@ -13,12 +13,20 @@ import { BlueNoiseTexture } from '../../../textures/BlueNoiseTexture.js';
 // References
 // - "Blue-Noise Dithered Sampling", Georgiev & Fajardo, SIGGRAPH 2016 Talks
 //   https://www.arnoldrenderer.com/research/dither_abstract.pdf
+// - Golden-ratio rotation of blue noise via Cranley-Patterson rotation:
+//   https://developer.nvidia.com/blog/rendering-in-real-time-with-spatiotemporal-blue-noise-textures-part-2/
 
-// construct the blue noise texture
+// constants
+// a maximum number of 100 effects is assumed per ray
 const BN_SIZE = 64;
-const bnTex = new BlueNoiseTexture( BN_SIZE, 1 );
-const texNode = texture( bnTex );
+const EFFECTS_PER_BOUNCE = 100;
+const GOLDEN_RATIO_FRACT = 0.61803398875;
+
+// construct nodes
+const blueNoiseTex = new BlueNoiseTexture( BN_SIZE, 1 );
+const blueNoiseTexNode = texture( blueNoiseTex );
 const pixelSeed = vec4( 0 ).toVar( 'blueDitherSeed' );
+const bounceIndex = uint( 0 ).toVar( 'blueDitherBounceIndex' );
 
 // When dithering, the sobol sampler is seeded with a constant pixel so every pixel
 // uses the same sequence, which is modified with the per-pixel blue noise sample.
@@ -26,17 +34,39 @@ const blueDitherInitFunc = wgslTagFn/* wgsl */`
 	fn blueDitherInitialize( pixel: vec2u, pathIndex: u32, bounceIndex: u32 ) -> void {
 
 		let coord = vec2i( pixel % vec2u( ${ BN_SIZE }u ) );
-		${ pixelSeed } = textureLoad( ${ texNode }, coord, 0 );
+		${ pixelSeed } = textureLoad( ${ blueNoiseTexNode }, coord, 0 );
+		${ bounceIndex } = bounceIndex;
 		${ rngInit }( vec2u( 0 ), pathIndex, bounceIndex );
 
 	}
 `;
 
+const blueDitherNextBounceFunc = wgslTagFn/* wgsl */`
+	fn blueDitherNextBounce() -> void {
+
+		${ bounceIndex }++;
+		${ rngNextBounce }();
+
+	}
+`;
+
+// The per-pixel scalar is rotated by the golden ratio per dimension. Apply a per-dimension
+// offset to reduce the corelation of the sequence.
 const blueDitherRand4Func = wgslTagFn/* wgsl */`
 	fn blueDitherRand4( effect: u32 ) -> vec4f {
 
+		// Get the dimension index of the current sample, rand rotate each element by a
+		// golden ratio Cranley-Patterson rotation value. Each sample is 4 floats so we
+		// apply a multiple of 4 so each element is unique.
+		let dimension = f32( ( ${ bounceIndex } * ${ EFFECTS_PER_BOUNCE }u + effect ) );
+		let dimensionVector = 4.0 * dimension + vec4f( 0.0, 1.0, 2.0, 3.0 );
+		let rotationVector = dimensionVector * ${ GOLDEN_RATIO_FRACT };
+
+		// Get the scrambled sobol stratified sample
 		let stratifiedSample = ${ rand4 }( effect );
-		return fract( stratifiedSample + ${ pixelSeed }.r );
+
+		// offset it by the blue noise seed and dimension rotation
+		return fract( stratifiedSample + ${ pixelSeed }.r + rotationVector );
 
 	}
 `;
@@ -66,9 +96,7 @@ const blueDitherRand1Func = wgslTagFn/* wgsl */`
 `;
 
 export {
-	// bounce increment is sobol pass through
-	rngNextBounce as rngNextBounce,
-
+	blueDitherNextBounceFunc as rngNextBounce,
 	blueDitherInitFunc as rngInit,
 	blueDitherRand1Func as rand1,
 	blueDitherRand2Func as rand2,
