1849fe2a45f29504581499a793818a8680ab5dca
Garrett Johnson <garrett.kjohnson@gmail.com>
2026-07-15 16:06:04 +0900

    Adjust the alphatest start point

--- src/webgpu/nodes/random.wgsl.js
+++ src/webgpu/nodes/random.wgsl.js
@@ -4,7 +4,7 @@ export const RNG_INDEX_ENVIRONMENT_SAMPLE = 1;
 export const RNG_INDEX_SCATTER_TYPE = 2;
 export const RNG_INDEX_SCATTER_DIRECTION = 3;
 export const RNG_INDEX_APERTURE_SAMPLE = 4;
-export const RNG_INDEX_ALPHA_TEST = 1000;
+export const RNG_INDEX_ALPHA_TEST = 50;
 
 // Re-exports used to adjust the random-sampling strategy to use
 
