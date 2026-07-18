c2adffb60a6caab5b8ed2719e439e7e43aa43cc8
Garrett Johnson <garrett.kjohnson@gmail.com>
2026-07-18 22:51:56 +0900

    Add context support

--- src/webgpu/nodes/random.wgsl.js
+++ src/webgpu/nodes/random.wgsl.js
@@ -5,9 +5,12 @@ export const RNG_INDEX_SCATTER_TYPE = 2;
 export const RNG_INDEX_SCATTER_DIRECTION = 3;
 export const RNG_INDEX_APERTURE_SAMPLE = 4;
 export const RNG_INDEX_ALPHA_TEST = 50;
+import { contextProxyFn } from 'three-mesh-bvh/webgpu';
+import * as sobol from './rand/sobol.wgsl.js';
 
-// Re-exports used to adjust the random-sampling strategy to use
-
-// export * from './rand/pcg.wgsl.js';
-export * from './rand/sobol.wgsl.js';
-// export * from './rand/bluedither.wgsl.js';
+export const rngInit = contextProxyFn( 'random.rngInit', sobol.rngInit );
+export const rngNextBounce = contextProxyFn( 'random.rngNextBounce', sobol.rngNextBounce );
+export const rand1 = contextProxyFn( 'random.rand1', sobol.rand1 );
+export const rand2 = contextProxyFn( 'random.rand2', sobol.rand2 );
+export const rand3 = contextProxyFn( 'random.rand3', sobol.rand3 );
+export const rand4 = contextProxyFn( 'random.rand4', sobol.rand4 );
