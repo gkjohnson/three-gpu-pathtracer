f80c6b48533c7989bd96ef6bfda50af96934c705
Garrett Johnson <garrett.kjohnson@gmail.com>
2026-07-18 23:19:47 +0900

    Export random function sets as constants

--- src/webgpu/index.js
+++ src/webgpu/index.js
@@ -1,6 +1,10 @@
 export * from './WebGPUPathTracer.js';
 export * from './BlurredEnvMapGenerator.js';
 
+export * as RANDOM_PCG from './nodes/rand/pcg.wgsl.js';
+export * as RANDOM_SOBOL from './nodes/rand/sobol.wgsl.js';
+export * as RANDOM_BLUE_DITHER from './nodes/rand/bluedither.wgsl.js';
+
 // extend the cameras to avoid adding WebGPU imports to the WebGLPathTracer
 import './shims/EquirectCameraShim.js';
 import './shims/PhysicalCameraShim.js';
