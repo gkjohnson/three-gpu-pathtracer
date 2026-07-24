b64961cbb6f97565052d8407de6fc0c9692e83dd
bright <1772127579@qq.com>
2026-07-24 11:37:48 +0800

    Pass dummy UV to turquinTexture to prevent missing attribute warning

--- example/renderVideo.js
+++ example/renderVideo.js
@@ -239,7 +239,7 @@ async function startRecording() {
 
 		videoSource = new CanvasSource( renderer.domElement, {
 			codec: 'vp9',
-			bitrate: 20_000_000, // high bitrate (20 Mbps)
+			bitrate: 2e7, // high bitrate
 			bitrateMode: 'constant',
 		} );
 
