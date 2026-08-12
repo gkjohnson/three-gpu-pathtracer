2a3f47fd1538dd9930a97a6f59e428b5b37a527d
Garrett Johnson <garrett.kjohnson@gmail.com>
2026-08-12 14:14:12 +0900

    ArrayCameraShim: Remove need for regenerating a new fn by modifying uniforms in place

--- src/webgpu/shims/ArrayCameraShim.js
+++ src/webgpu/shims/ArrayCameraShim.js
@@ -1,86 +1,67 @@
 import { ArrayCamera, Matrix4, Vector4, WebGPUCoordinateSystem } from 'three';
-import { uniformArray } from 'three/tsl';
+import { uniformArray, int } from 'three/tsl';
 import { ndcToCameraRay, rayStruct, wgslTagFn } from 'three-mesh-bvh/webgpu';
 
 ArrayCamera.prototype.getCameraRayFn = function getCameraRayFn() {
 
-	let cameraCount = 0;
-	let invViewProjectionMatrices;
-	let viewports;
 	const result = { fn: null, update: null };
 
-	const rebuild = () => {
+	const cameraCount = int( 0 );
+	const invViewProjectionMatrices = uniformArray( [], 'mat4' );
+	const viewports = uniformArray( [], 'vec4' );
 
-		cameraCount = this.cameras.length;
-		if ( cameraCount === 0 ) {
+	result.fn = wgslTagFn/* wgsl */`
+		fn getCameraRay( uv: vec2f, resolution: vec2f, ray: ptr<function, ${ rayStruct }> ) -> bool {
 
-			result.fn = wgslTagFn/* wgsl */`
-				fn getCameraRay( uv: vec2f, resolution: vec2f, ray: ptr<function, ${ rayStruct }> ) -> bool {
+			let pixel = uv * resolution;
 
-					return false;
+			for ( var i = 0u; i < ${ cameraCount }u; i ++ ) {
 
-				}
-			`;
-			return;
+				let viewport = ${ viewports }[ i ];
+				let viewportMax = viewport.xy + viewport.zw;
+				if ( all( pixel >= viewport.xy ) && all( pixel < viewportMax ) ) {
 
-		}
-
-		invViewProjectionMatrices = uniformArray(
-			Array.from( { length: cameraCount }, () => new Matrix4() ),
-			'mat4',
-		);
-		viewports = uniformArray(
-			Array.from( { length: cameraCount }, () => new Vector4() ),
-			'vec4',
-		);
-
-		result.fn = wgslTagFn/* wgsl */`
-			fn getCameraRay( uv: vec2f, resolution: vec2f, ray: ptr<function, ${ rayStruct }> ) -> bool {
-
-				let pixel = uv * resolution;
-
-				for ( var i = 0u; i < ${ cameraCount }u; i ++ ) {
-
-					let viewport = ${ viewports }[ i ];
-					let viewportMax = viewport.xy + viewport.zw;
-					if ( all( pixel >= viewport.xy ) && all( pixel < viewportMax ) ) {
-
-						let cameraUv = ( pixel - viewport.xy ) / viewport.zw;
-						let ndc = cameraUv * 2.0 - vec2f( 1.0 );
-						let cameraRay = ${ ndcToCameraRay }( ndc, ${ invViewProjectionMatrices }[ i ] );
-						ray.origin = cameraRay.origin;
-						ray.direction = cameraRay.direction;
-						return true;
-
-					}
+					let cameraUv = ( pixel - viewport.xy ) / viewport.zw;
+					let ndc = cameraUv * 2.0 - vec2f( 1.0 );
+					let cameraRay = ${ ndcToCameraRay }( ndc, ${ invViewProjectionMatrices }[ i ] );
+					ray.origin = cameraRay.origin;
+					ray.direction = cameraRay.direction;
+					return true;
 
 				}
-
-				return false;
 
 			}
-		`;
 
-	};
+			return false;
+
+		}
+	`;
 
 	result.update = () => {
 
-		let needsRebuild = false;
-		if ( this.cameras.length !== cameraCount ) {
+		const { cameras } = this;
+		const needsRebuild = cameraCount.node.value !== cameras.length;
+		cameraCount.node.value = cameras.length;
 
-			rebuild();
-			needsRebuild = true;
+		while ( viewports.array.length < cameras.length ) {
+
+			viewports.array.push( new Vector4() );
+			invViewProjectionMatrices.array.push( new Matrix4() );
 
 		}
 
-		for ( let i = 0; i < cameraCount; i ++ ) {
+		invViewProjectionMatrices.array.length = cameras.length;
+		viewports.array.length = cameras.length;
 
-			const camera = this.cameras[ i ];
+		for ( let i = 0; i < cameras.length; i ++ ) {
+
+			const camera = cameras[ i ];
 			const viewport = camera.viewport;
 
 			camera.coordinateSystem = WebGPUCoordinateSystem;
 			camera.updateMatrixWorld();
 			camera.updateProjectionMatrix();
+
 			invViewProjectionMatrices.array[ i ].multiplyMatrices( camera.matrixWorld, camera.projectionMatrixInverse );
 			viewports.array[ i ].copy( viewport );
 
@@ -90,7 +71,8 @@ ArrayCamera.prototype.getCameraRayFn = function getCameraRayFn() {
 
 	};
 
-	rebuild();
+	result.update();
+
 	return result;
 
 };
