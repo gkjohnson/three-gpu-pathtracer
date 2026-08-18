a6943a8a3a6c2a6333c6cc7a776f34e2a8756ae1
Garrett Johnson <garrett.kjohnson@gmail.com>
2026-08-18 17:54:26 +0900

    Update description

--- example/index.js
+++ example/index.js
@@ -43,6 +43,8 @@ const DEVICE_LIMITS_REQUESTED = [
 	'maxStorageBufferBindingSize',
 ];
 
+const DESCRIPTION = 'Drag and drop a GLTF, GLB, DAE, or MPD file to view it.';
+
 const DRACO_DECODER_PATH = 'https://www.gstatic.com/draco/versioned/decoders/1.5.7/';
 const KTX2_TRANSCODER_PATH = 'https://cdn.jsdelivr.net/npm/three@0.181.1/examples/jsm/libs/basis/';
 const MODEL_FILE_REGEX = /\.(gltf|glb|dae|mpd)$/i;
@@ -165,6 +167,7 @@ async function init() {
 
 	loader = new LoaderElement();
 	loader.attach( document.body );
+	loader.setDescription( DESCRIPTION );
 
 	// adapter limits
 	const adapter = await navigator.gpu?.requestAdapter();
